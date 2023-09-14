fc706af4e89d153ffe7976ff6ced0ab9519255df
Giuseppe Caliendo <giuseppe.caliendo@azzurrodigitale.com>
2023-09-14 21:06:48 +0200

    first commit

---
+++ init.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget https://gpt4all.io/models/ggml-gpt4all-j.bin -O models/ggml-gpt4all-j
+wget https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML/resolve/main/llama-2-7b-chat.ggmlv3.q6_K.bin -O models/llma2-7b
